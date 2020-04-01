7d593ba594fd378205340fb0bd4628d91cfcd5b6
brianqian <qian.brian@gmail.com>
2020-04-01 15:20:49 -0400

    started frontpage layout and components

--- client/src/pages/App.js
+++ client/src/pages/App.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import styled from 'styled-components/macro';
 import EditProfile from './EditProfile';
+import Header from '../components/Header';
+import Home from './Home';
+import { Switch, Route } from 'react-router-dom';
 
 const Container = styled.div``;
 
 function App() {
   return (
     <Container>
-      <EditProfile />
+      <Header />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/profile" component={EditProfile} />
+      </Switch>
     </Container>
   );
 }
