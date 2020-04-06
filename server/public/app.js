d7919ccab659431be20d220aaf6d2d04c17ed77f
Evan Tatay-Hinds <gaurdianaq@pop-os.localdomain>
2020-04-06 13:44:13 -0400

    stuff

--- server/public/app.js
+++ server/public/app.js
@@ -1,14 +1,18 @@
-const client = feathers();
-
-client.configure(feathers.rest.fetch(window.fetch));
-
-client.configure(feathers.authentication({
-  storage: window.localStorage
-}));
-
-async function login1()
-{
-  console.log(client.service('users').find());
+function login1() {
+  fetch('/authentication',
+  {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      strategy: 'local',
+      email: 'hello@feathersjs.com',
+      password: 'supersecret'
+    })
+  }).then(response => {
+    console.log(response);
+  });
 }
 
 function login2()
