5af9b81c006492caf9a02c42d7a5e478a636cb02
Evan Tatay-Hinds <gaurdianaq@pop-os.localdomain>
2020-04-13 21:43:36 -0400

    added dotenv

--- server/src/main/kotlin/org/covid19support/Application.kt
+++ server/src/main/kotlin/org/covid19support/Application.kt
@@ -1,13 +1,14 @@
 package org.covid19support
 
+import io.github.cdimascio.dotenv.Dotenv
 import io.ktor.application.*
 import io.ktor.features.*
 import io.ktor.gson.gson
-import io.ktor.http.ContentType
 
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
 fun Application.main() {
+    dotenv = Dotenv.load()
     install(DefaultHeaders)
     install(ContentNegotiation) {
         gson {
--- server/src/main/kotlin/org/covid19support/DbSettings.kt
+++ server/src/main/kotlin/org/covid19support/DbSettings.kt
@@ -4,7 +4,7 @@ import org.jetbrains.exposed.sql.Database
 
 object DbSettings {
    val db by lazy {
-       Database.connect("jdbc:postgresql://localhost/skill_marketplace", driver = "org.postgresql.Driver",
-               user = "gaurdianaq", password = "password")
+       Database.connect("jdbc:postgresql://"+ dotenv["DB_HOST"]+"/"+ dotenv["DB_NAME"], driver = "org.postgresql.Driver",
+               user = dotenv["DB_USER"]!!, password = dotenv["DB_PASSWORD"]!!)
    }
 }
