8da7867d52d3abe6360be0f339872d894a9e485d
Evan Tatay-Hinds <gaurdianaq@pop-os.localdomain>
2020-04-15 19:46:12 -0400

    authentication seems to work

--- server/src/main/kotlin/org/covid19support/modules/courses/module.kt
+++ server/src/main/kotlin/org/covid19support/modules/courses/module.kt
@@ -1,9 +1,13 @@
 package org.covid19support.modules.courses
 
+import com.auth0.jwt.interfaces.DecodedJWT
 import io.ktor.application.*
 import io.ktor.routing.*
 import io.ktor.response.*
+import io.ktor.sessions.*
 import org.covid19support.DbSettings
+import org.covid19support.SessionAuth
+import org.covid19support.modules.authentication.Token
 import org.jetbrains.exposed.sql.*
 import org.jetbrains.exposed.sql.transactions.transaction
 
@@ -12,6 +16,37 @@ fun Application.courses_module() {
         get("/courses") {
             val courses: MutableList<Course> = mutableListOf<Course>()
             call.respondText("Not Implemented Yet!")
+        }
+
+        get("/courses/test") {
+            val token: String? = call.sessions.get<SessionAuth>()?.token
+            if (token != null)
+            {
+                val decodedToken: DecodedJWT? = Token.verify(token)
+                if (decodedToken != null) {
+                    call.respond("authenticated!")
+                }
+                else {
+                    call.respond("SHAME, sending a fake token!")
+                }
+            }
+            else {
+                call.respond("Please login!")
+            }
+        }
+
+        post("/courses") {
+           val token: String? = call.sessions.get<SessionAuth>()?.token
+            if (token != null)
+            {
+                val decodedToken: DecodedJWT? = Token.verify(token)
+                if (decodedToken != null) {
+                    //decodedToken.
+                }
+                else {
+
+                }
+            }
         }
     }
 }
